2d504014f657c7aa3beeabf11c3fe33121bd2604
q <corey@qr7.com>
2011-09-05 13:23:30 -0700

    ezmode

--- lolaws/local_settings_TEMPLATE.py
+++ lolaws/local_settings_TEMPLATE.py
@@ -1,10 +1,13 @@
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'lolaws',
         'USER': 'lolaws',
-        'PASSWORD': 'SETME',
-        'HOST': 'SETME',
+        'PASSWORD': 'SETME', # Exercise 2
+        'HOST': 'SETME', # Exercise 2
         'PORT': '',
     }
 }
@@ -13,12 +16,12 @@ AWS_ACCESS_KEY_ID = "SETME"
 AWS_SECRET_ACCESS_KEY = "SETME"
 AWS_STORAGE_BUCKET_NAME = "SETME"
 
-# S3
-STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-DEFAULT_FILE_STORAGE = 'storages.backends.s3.S3Storage'
+# S3 - Exercise 3
+#STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+#DEFAULT_FILE_STORAGE = 'storages.backends.s3.S3Storage'
 
-STATIC_URL = 'http://'+AWS_STORAGE_BUCKET_NAME+'.s3.amazonaws.com/'
-ADMIN_MEDIA_PREFIX = STATIC_URL + "admin/"
+#STATIC_URL = 'http://'+AWS_STORAGE_BUCKET_NAME+'.s3.amazonaws.com/'
+#ADMIN_MEDIA_PREFIX = STATIC_URL + "admin/"
 
 AWS_HEADERS = {
     'Cache-Control': 'max-age=86400',
@@ -31,6 +34,3 @@ AWS_CALLING_FORMAT = CallingFormat.SUBDOMAIN
 EMAIL_BACKEND = 'django_ses.SESBackend'
 DEFAULT_FROM_EMAIL = 'lolaws <SETME@EMAIL.LOL>'
 EMAIL_SUBJECT_PREFIX = '[lolaws] '
-
-
-
