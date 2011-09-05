8fc68ce057de9b3f8ec1c771ec869c1d6a3c8e71
Shoe <schomaker.c@gmail.com>
2011-09-05 05:05:29 -0400

    asdf

--- lolaws/settings.py
+++ lolaws/settings.py
@@ -62,8 +62,7 @@ STATIC_ROOT = "static_files/"
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-#STATIC_URL = 'http://BUCKETNAME.s3.amazonaws.com/'
-STATIC_URL = '/static/'
+STATIC_URL = 'http://BUCKETNAME.s3.amazonaws.com/'
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
@@ -123,9 +122,8 @@ INSTALLED_APPS = (
     'django.contrib.admindocs',
 
     # external
-    #'storages',
-    #'django_ses',
-    'django_extensions',
+    'storages',
+    'django_ses',
 
     # disapp
     'lolaws.core',
@@ -168,7 +166,7 @@ LOGGING = {
 #AWS_STORAGE_BUCKET_NAME = "SETME"
 #STATIC_URL = 'http://'+AWS_STORAGE_BUCKET_NAME+'.s3.amazonaws.com/'
 #ADMIN_MEDIA_PREFIX = STATIC_URL + "admin/"
-#from S3 import CallingFormat
+from S3 import CallingFormat
 #AWS_CALLING_FORMAT = CallingFormat.SUBDOMAIN
 #AWS_HEADERS = {
 #    'Cache-Control': 'max-age=86400',
--- lolaws/uploadr/urls.py
+++ lolaws/uploadr/urls.py
@@ -1,8 +1,19 @@
+from django.conf import settings
 from django.conf.urls.defaults import patterns, url
+
 from lolaws.uploadr.views import *
 
 urlpatterns = patterns('',
     url(r'^$', view=upload, name='uploadr-upload'),
     url(r'^success/(?P<image_id>\d+)/', view=success, name='uploadr-success'),
     url(r'^all/$', view=all, name='uploadr-all'),
+)
+
+_media_url = settings.MEDIA_URL
+if _media_url.startswith('/'):
+    _media_url = _media_url[1:]
+urlpatterns += patterns('',
+    # Serve a favicon, to get rid of those irritating 404 log statements
+    (r'^{0}(?P<path>.*)$'.format(_media_url), 'django.views.static.serve',
+        {'document_root': settings.MEDIA_ROOT}),
 )
